95efaf04ca3f2a198161794b5bc507fd77843068
cocoataster <cocoataster@gmail.com>
2019-02-14 23:12:29 +0100

    Add Season and Episode tests implementation

--- WesterosTests/EpisodeTests.swift
+++ WesterosTests/EpisodeTests.swift
@@ -11,21 +11,55 @@ import XCTest
 
 class EpisodeTests: XCTestCase {
     
+    var season: Season?
+    var episode: Episode!
+    
     var date: Date!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        let dateComponents = DateComponents(calendar: Calendar.current, timeZone: TimeZone.current,  year: 2018, month: 8, day: 22)
-        let calendar = Calendar.current
-        date = calendar.date(from: dateComponents)
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        date = formatter.date(from: "2011-04-24")
+        
+        season = Season(name: "Season1", releaseDate: date)
+        episode = Episode(title: "Episode1", airDate: date, season: season)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func testEpisodeExistence() {
-        let episode = Episode(title: "Episode1", airDate: date, season: Season(name: "Season1", releaseDate: date))
+    func testEpisode_Existence() {
         XCTAssertNotNil(episode)
+    }
+    
+    func testEpisode_IsCustomStringConvertible() {
+        XCTAssertNotNil(episode.description)
+    }
+    
+    func testEpisode_IsEquatable() {
+        // Identity
+        XCTAssertEqual(episode, episode)
+        
+        // Equality
+        let sameEpisode = Episode(title: "Episode1", airDate: date, season: season)
+        XCTAssertEqual(episode, sameEpisode)
+        
+        // Non-Equality
+        let diffEpisode = Episode(title: "Episode2", airDate: date, season: season)
+        XCTAssertNotEqual(episode, diffEpisode)
+    }
+    
+    func testEpisode_IsHashable() {
+        XCTAssertNotNil(episode.hashValue)
+    }
+    
+    func testEpisode_IsComparable() {
+        // Approx. 5.200.000 seconds from Episode1 to Episode10
+        let seconds = 5_200_000
+        let newDate = date.addingTimeInterval(TimeInterval(seconds))
+        let lastEpisode = Episode(title: "Episode10", airDate: newDate, season: season)
+        XCTAssertLessThan(episode, lastEpisode)
     }
 }
---
+++ WesterosTests/SeasonTests.swift
@@ -0,0 +1,78 @@
+//
+//  SeasonTests.swift
+//  WesterosTests
+//
+//  Created by Eric Sans Alvarez on 14/02/2019.
+//  Copyright © 2019 Eric Sans Alvarez. All rights reserved.
+//
+
+import XCTest
+@testable import Westeros
+
+class SeasonTests: XCTestCase {
+    
+    var season: Season!
+    var firstEpisode: Episode!
+    var lastEpisode: Episode!
+    
+    var firstDate: Date!
+    var lastDate: Date!
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        firstDate = formatter.date(from: "2011-04-24")
+        lastDate = formatter.date(from: "2011-06-19")
+        
+        season = Season(name: "Season1", releaseDate: firstDate)
+        firstEpisode = Episode(title: "\"Winter Is Coming\"", airDate: firstDate, season: season)
+        lastEpisode = Episode(title: "\"Fire and Blood\"", airDate: lastDate, season: season)
+        
+        
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testSeason_Existence() {
+        XCTAssertNotNil(season)
+    }
+    
+    func testSeason_AddEpisodes() {
+        XCTAssertEqual(season.count, 0)
+        season.add(episodes: firstEpisode, lastEpisode)
+        XCTAssertEqual(season.count, 2)
+    }
+    
+    func testSeason_IsCustomStringConvertible() {
+        XCTAssertNotNil(season.description)
+    }
+    
+    func testSeason_IsEquatable() {
+        // Identity
+        XCTAssertEqual(season, season)
+        
+        // Equality
+        let sameSeason = Season(name: "Season1", releaseDate: firstDate)
+        XCTAssertEqual(season, sameSeason)
+        
+        // Non-Equality
+        let secondSeason = Season(name: "Season2", releaseDate: lastDate)
+        XCTAssertNotEqual(season, secondSeason)
+    }
+    
+    func testSeason_IsHashable() {
+        XCTAssertNotNil(season.hashValue)
+    }
+    
+    func testSeason_IsComparable() {
+        // Approx. 31.558.000 seconds from Season1 to Season2
+        let seconds = 31_557_600
+        let newDate = firstDate.addingTimeInterval(TimeInterval(seconds))
+        let secondSeason = Season(name: "Season2", releaseDate: newDate)
+        XCTAssertLessThan(season, secondSeason)
+    }
+
+}
